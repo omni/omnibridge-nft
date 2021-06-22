ae6991d1ccfc59d2641a94f11e36373922b791f2
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-06-22 17:07:35 +0400

    Add unit tests for tokens briding via metatransactions

--- test/omnibridge_nft/common.test.js
+++ test/omnibridge_nft/common.test.js
@@ -208,11 +208,54 @@ function runTests(accounts, isHome) {
       const method = contract.methods['relayToken(address,address,uint256)']
       return method(token.address, user2, id, { from: user }).then(() => user2)
     },
+    async function metaTransactionRelay(tokenId) {
+      const id = tokenId || (await mintNewERC721())
+      const nonce = await token.nonces(user)
+
+      const typedData = {
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'version', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+            { name: 'verifyingContract', type: 'address' },
+          ],
+          metaSafeTransferFrom: [
+            { name: 'from', type: 'address' },
+            { name: 'to', type: 'address' },
+            { name: 'tokenId', type: 'uint256' },
+            { name: 'data', type: 'bytes' },
+            { name: 'nonce', type: 'uint256' },
+          ],
+        },
+        domain: {
+          name: 'MetaTxERC721',
+          version: '1',
+          chainId: 1337,
+          verifyingContract: token.address,
+        },
+        primaryType: 'metaSafeTransferFrom',
+        message: {
+          from: user,
+          to: contract.address,
+          tokenId: id,
+          data: '0x',
+          nonce,
+        },
+      }
+
+      const sig = await web3.eth.signTypedData(user, typedData)
+      return token.metaSafeTransferFrom(user, contract.address, id, '0x', nonce, `${sig}01`).then(() => user)
+    },
   ]
 
   before(async () => {
     tokenImageERC721 = await ERC721BridgeToken.new()
     tokenImageERC1155 = await ERC1155BridgeToken.new()
+    web3.extend({
+      property: 'eth',
+      methods: [{ name: 'signTypedData', call: 'eth_signTypedData', params: 2 }],
+    })
   })
 
   beforeEach(async () => {
@@ -1592,6 +1635,52 @@ function runTests(accounts, isHome) {
 
             expect(await token.balanceOf(contract.address, 1)).to.be.bignumber.equal(ZERO)
             expect(await token.balanceOf(contract.address, 2)).to.be.bignumber.equal(ZERO)
+          })
+
+          it('should make calls to handleNativeNFT when using meta transactions', async () => {
+            const nonce = await token.nonces(user)
+
+            const typedData = {
+              types: {
+                EIP712Domain: [
+                  { name: 'name', type: 'string' },
+                  { name: 'version', type: 'string' },
+                  { name: 'chainId', type: 'uint256' },
+                  { name: 'verifyingContract', type: 'address' },
+                ],
+                metaSafeTransferFrom: [
+                  { name: 'from', type: 'address' },
+                  { name: 'to', type: 'address' },
+                  { name: 'id', type: 'uint256' },
+                  { name: 'amount', type: 'uint256' },
+                  { name: 'data', type: 'bytes' },
+                  { name: 'nonce', type: 'uint256' },
+                ],
+              },
+              domain: {
+                name: 'MetaTxERC1155',
+                version: '1',
+                chainId: 1337,
+                verifyingContract: token.address,
+              },
+              primaryType: 'metaSafeTransferFrom',
+              message: {
+                from: user,
+                to: contract.address,
+                id: 1,
+                amount: 2,
+                data: '0x',
+                nonce,
+              },
+            }
+
+            const sig = await web3.eth.signTypedData(user, typedData)
+            await token.metaSafeTransferFrom(user, contract.address, 1, 2, '0x', nonce, `${sig}01`)
+
+            const events = await getEvents(ambBridgeContract, { event: 'MockedEvent' })
+            expect(events.length).to.be.equal(1)
+            const depositEvents = await getEvents(contract, { event: 'TokensBridgingInitiated' })
+            expect(depositEvents.length).to.be.equal(1)
           })
 
           describe('fixFailedMessage', () => {
